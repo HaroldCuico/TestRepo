ed830c7fe82809bf2ce4383e46158e0f1cf9a268
jaymar921 <jayharronabejar@gmail.com>
2022-09-13 12:57:42 +0800

    Added Coffee

---
+++ ConsoleApp1TestRepo/ConsoleApp1TestRepo/Coffee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1TestRepo
+{
+    class Coffee
+    {
+        private int volume;
+        private string type;
+        private bool hot;
+
+        public int Volume { get; set; }
+        public string Type { get; set; }
+        public bool Hot { get; set; }
+
+        public string toString() { return $"{type} - {volume}L - Hot: {hot}"; }
+
+    }
+}
