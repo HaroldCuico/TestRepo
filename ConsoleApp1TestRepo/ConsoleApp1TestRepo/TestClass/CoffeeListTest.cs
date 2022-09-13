a070190ddafdd25a31885b45583bb2ec1ef18238
JayMar <72720429+jaymar921@users.noreply.github.com>
2022-09-14 07:52:34 +0800

    Added Delete coffee and get coffee feature in CoffeeList

---
+++ ConsoleApp1TestRepo/ConsoleApp1TestRepo/TestClass/CoffeeListTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1TestRepo.TestClass
+{
+    [TestClass]
+    public class CoffeeListTest
+    {
+        public void ShouldAddCoffee() {
+            var coffeeList = new CoffeeList();
+
+            Coffee coffee = new Coffee(0.2, "Vanilla Iced Coffee", false);
+
+            coffeeList.addCoffee(coffee);
+            Assert.AreEqual(coffeeList.GetCoffee(0), coffee);
+        }
+    }
+}
