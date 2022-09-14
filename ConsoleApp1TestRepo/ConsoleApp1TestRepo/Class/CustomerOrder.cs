b985ba625394575ea7b8410d1ac2bf6f1c94d496
yato0 <jdcaramonte159@gmail.com>
2022-09-14 08:59:18 +0800

    Updating methods in CustomerOrder and Adding menu order

--- ConsoleApp1TestRepo/ConsoleApp1TestRepo/Class/CustomerOrder.cs
+++ ConsoleApp1TestRepo/ConsoleApp1TestRepo/Class/CustomerOrder.cs
@@ -8,16 +8,50 @@ namespace ConsoleApp1TestRepo.Class
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public CoffeeList ListOfCoffee { get; set; }
+        public Coffee Coffee { get; set; }
 
-        public void AddCoffee(Coffee c)
+
+        public CustomerOrder()
         {
-            ListOfCoffee.addCoffee(c);  
+            Id++;   
         }
 
-        public void GetCoffeeList()
+        public void Menu()
         {
-            ListOfCoffee.displayAllCoffee();
+            List<string> coffees = new List<string> { "Cappuccino", "Latte", "Mocha", "Americano" };
+
+            Console.Write($"Please enter your name: ");
+
+            this.Name = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine($"Please choose your coffee.");
+            for (int i = 0; i < coffees.Count; i++)
+            {
+                Console.WriteLine($"({i + 1}) - {coffees[i]}");
+            }
+
+            Console.Write($"Please enter the number:");
+
+            int coffeeType = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            Console.WriteLine();
+            Console.WriteLine($"(1) Hot");
+            Console.WriteLine($"(2) Cold");
+            Console.Write($"Please choose: ");
+
+
+            bool coffeeTemp = Convert.ToInt32(Console.ReadLine()) == 1 ? true : false;
+
+
+             //Temporarily Setting the volume to 0.5
+            this.Coffee = new Coffee(0.5, coffees[coffeeType], coffeeTemp);
+        }
+
+        public void GetOrder()
+        {
+            Console.WriteLine()
+            Console.WriteLine($"{this.Name} order is {Coffee}");
         }
     }
 }
