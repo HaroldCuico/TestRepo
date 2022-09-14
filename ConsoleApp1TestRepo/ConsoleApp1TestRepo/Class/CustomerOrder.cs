cd278408b8ba23391a5d2de0573390bee9eb3c7e
yato0 <jdcaramonte159@gmail.com>
2022-09-14 08:06:55 +0800

    Initializing CustomerOrderFeature

---
+++ ConsoleApp1TestRepo/ConsoleApp1TestRepo/Class/CustomerOrder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1TestRepo.Class
+{
+    public class CustomerOrder
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        CoffeeList ListOfCoffee { get; set; }
+
+        public void GetCoffeeList()
+        {
+            ListOfCoffee.displayAllCoffee();
+        }
+    }
+}
